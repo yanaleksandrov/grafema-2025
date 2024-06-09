e5fe51ca98fa0f5a76aec30036da8bfc784c240b
yanaleksandrov <yanalexandrov@gmail.com>
2024-06-10 01:34:59 +0300

    Init

--- globals.js
+++ globals.js
@@ -6,7 +6,7 @@ module.exports = {
       btnIcon: '<svg xmlns="http://www.w3.org/2000/svg" width="19" height="18" fill="none"><path stroke="#2E3238" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M7.7 5.3 11.4 9l-3.7 3.8"/></svg>',
     },
     title: "Создавайте сайты вместе с Grafema CMS",
-    description: "Многоцелевая платформа с открытым исходным кодом для разработки веб сайтов и приложений.",
+    description: "Многоцелевая «headless» платформа с открытым исходным кодом для разработки веб сайтов и приложений",
     buttons: [
       {
         text: "Free Download",
